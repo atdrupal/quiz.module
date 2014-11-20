6c8033bcc8d500bbd399111cde34e1a2af0e8830
Andy Truong <thehongtt@gmail.com>
2014-11-20 14:40:07 +0700

    Fix broken FeedbackHelper::canAccess().

--- src/Helper/Quiz/FeedbackHelper.php
+++ src/Helper/Quiz/FeedbackHelper.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\quiz\Helper\Quiz;
 
-use Drupal\quiz\Entity\Result;
-
 class FeedbackHelper {
 
   /**
@@ -29,15 +27,16 @@ class FeedbackHelper {
    * Menu access check for question feedback.
    */
   public function canAccess($quiz, $page_number) {
-    if ($page_number <= 0 || !array_filter($quiz->review_options['question'])) {
+    if (($page_number <= 0) || !array_filter($quiz->review_options['question'])) {
       return FALSE;
     }
 
     $result_id = empty($_SESSION['quiz'][$quiz->qid]['result_id']) ? $_SESSION['quiz']['temp']['result_id'] : $_SESSION['quiz'][$quiz->qid]['result_id'];
     $result = quiz_result_load($result_id);
+    $question_vid = $result->layout[$page_number]['vid'];
 
     return quiz_answer_controller()
-        ->loadByResultAndQuestion($result_id, $result->layout[$page_number]['vid']);
+        ->loadByResultAndQuestion($result_id, $question_vid);
   }
 
 }
