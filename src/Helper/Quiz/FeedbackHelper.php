00ffb7e70125c0a152d5635b4c1400837a99d7bf
Andy Truong <thehongtt@gmail.com>
2014-11-20 13:19:12 +0700

    Move FeedbackHelper::canReview() to Result::canReview().

--- src/Helper/Quiz/FeedbackHelper.php
+++ src/Helper/Quiz/FeedbackHelper.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\quiz\Helper\Quiz;
 
+use Drupal\quiz\Entity\Result;
+
 class FeedbackHelper {
 
   /**
@@ -45,31 +47,6 @@ class FeedbackHelper {
       if (quiz_answer_controller()->loadByResultAndQuestion($result_id, $qinfo['vid'])) {
         return TRUE;
       }
-    }
-  }
-
-  /**
-   * Can the quiz taker view the requested review?
-   *
-   * There's a workaround in here: @kludge
-   *
-   * When review for the question is enabled, and it is the last question,
-   * technically it is the end of the quiz, and the "end of quiz" review settings
-   * apply. So we check to make sure that we are in question taking and the
-   * feedback is viewed within 5 seconds of completing the question/quiz.
-   */
-  public function canReview($option, $result) {
-    $quiz = quiz_load($result->quiz_qid, $result->quiz_vid);
-
-    // Check what context the result is in.
-    if ($result->time_end && arg(2) !== 'take') {
-      // Quiz is over. Pull from the "at quiz end" settings.
-      return !empty($quiz->review_options['end'][$option]);
-    }
-
-    // Quiz ongoing. Pull from the "after question" settings.
-    if (!$result->time_end || $result->time_end >= REQUEST_TIME - 5) {
-      return !empty($quiz->review_options['question'][$option]);
     }
   }
 
